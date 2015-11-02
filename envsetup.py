0e2e21c4c5daf870f4b44107a1ed2fe9bb525d1b
Onegin Liang(梁伯達_Pegatron) <Onegin_Liang@pega.corp.pegatron>
2015-11-02 09:50:20 +0800

    envsetup: add "-f" option to enforce re-create an existed index
    Signed-off-by: Onegin Liang(梁伯達_Pegatron) <Onegin_Liang@pega.corp.pegatron>

--- envsetup.py
+++ envsetup.py
@@ -3,8 +3,18 @@ from elasticsearch import RequestError
 from elasticsearch import ConnectionTimeout
 from elasticsearch.client import IndicesClient
 import json
+import sys
+import getopt
 
-def setup():
+def setup(forced):
+	es = Elasticsearch([{'host': 'localhost', 'port': 9200}], max_retries=10, retry_on_timeout=True)
+	idx_client = IndicesClient(es)
+	if (idx_client.exists(index='max1')):
+		if (forced):
+			idx_client.delete(index='max1')
+		else :
+			print "Index already exists!"
+			return
 	csv_status = {"csv_status" : {"path_match": "*.STATUS", "mapping": {"index": "not_analyzed"}}}
 	csv_value = {"csv_value" : {"path_match": "*.VALUE", "mapping": {"index": "not_analyzed"}}}
 	csv_u_limit = {"csv_u_limit" : {"path_match": "*.U_LIMIT", "mapping": {"index": "not_analyzed"}}}
@@ -16,9 +26,22 @@ def setup():
 	mappings = {"dynamic_templates" : dynamic_templates, "properties" : properties}
 	data = {"mappings" : {"mp": mappings}}
 	print json.dumps(data)
-	es = Elasticsearch([{'host': 'localhost', 'port': 9200}], max_retries=10, retry_on_timeout=True)
-	idx_client = IndicesClient(es)
 	idx_client.create(index='max1', body=data)
 
+def usage():
+	return
+
+def main(argv):
+	forced = 0
+	try:
+		opts, args = getopt.getopt(argv, "f")
+	except getopt.GetoptError:
+		usage()
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == "-f":
+			forced = 1
+	setup(forced)
+
 if __name__ == '__main__':
-	setup()
+	main(sys.argv[1:])
